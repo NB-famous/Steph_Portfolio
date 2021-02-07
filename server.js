f3cec6c69315e10ec89e19c4e047c6e070307b5d
NB-famous <ncbadoles@gmail.com>
2021-02-07 15:17:39 -0500

    completed template for stephs portfolio but only works through localhost 3002

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 const port=3002;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
+app.get('/p1', (req, res) => res.sendFile(path.join(__dirname, '/portfolioItem.html')))
+app.get('/js', (req, res) => res.sendFile(path.join(__dirname, '/js/app.js')))
 app.get('/css', (req, res) => res.sendFile(path.join(__dirname, '/css/style.css')))
+
 
 //this will allow to serve images//
 app.use(express.static(__dirname + '/public'));
