3828c804f3f4832d6cc59fe6559c78d02a19b5b8
NB-famous <ncbadoles@gmail.com>
2021-02-07 20:00:26 -0500

    added proc file to deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port=3002;
+const port=process.env.PORT || 3002;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 app.get('/p1', (req, res) => res.sendFile(path.join(__dirname, '/portfolioItem.html')))
