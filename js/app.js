2159d440abc1154b936a5fb2aefc7ab8714672fa
NB-famous <ncbadoles@gmail.com>
2021-02-19 21:16:39 -0500

    updated website fix bugs due to picture proportions

--- js/app.js
+++ js/app.js
@@ -63,3 +63,20 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+//////////////////// Sticky Header functions ////////////
+
+
+window.onscroll = function() {myFunction()};
+
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
